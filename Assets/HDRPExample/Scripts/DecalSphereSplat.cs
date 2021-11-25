09e2bfe9974a96f72b452271d3cfe8a4e242a1ea
naelstrof <naelstrof@gmail.com>
2021-11-25 03:20:54 -0700

    Now no longer manually maintain lifetimes of decals, no more memory allocations.

--- Assets/HDRPExample/Scripts/DecalSphereSplat.cs
+++ Assets/HDRPExample/Scripts/DecalSphereSplat.cs
@@ -7,6 +7,15 @@ public class DecalSphereSplat : MonoBehaviour {
     public Material projector;
     public Color color;
     private static Collider[] colliders = new Collider[32];
+    private static List<Renderer> staticRenderers = new List<Renderer>();
+    private static List<Renderer> staticTempRenderers = new List<Renderer>();
+    private void GetComponentsInChildrenNoAlloc<T>(Transform t, List<T> temp, List<T> result) {
+        t.GetComponents<T>(temp);
+        result.AddRange(temp);
+        for(int i=0;i<t.childCount;i++) {
+            GetComponentsInChildrenNoAlloc<T>(t.GetChild(i), temp, result);
+		}
+	}
     void Start() {
         projector = Material.Instantiate(projector);
         projector.color = color;
@@ -16,8 +25,11 @@ public class DecalSphereSplat : MonoBehaviour {
         int hits = Physics.OverlapSphereNonAlloc(transform.position, scale, colliders, hitMask, QueryTriggerInteraction.UseGlobal);
         for(int i=0;i<hits;i++) {
             Collider c = colliders[i];
-            foreach(Renderer r in c.transform.root.GetComponentsInChildren<Renderer>()) {
-                PaintDecal.instance.RenderDecal(r, projector, transform.position-Vector3.forward*scale*0.5f, Quaternion.identity, Vector2.one*scale*0.5f, scale);
+
+            staticRenderers.Clear();
+            GetComponentsInChildrenNoAlloc<Renderer>(c.transform.root, staticTempRenderers, staticRenderers);
+            foreach(Renderer r in staticRenderers) {
+                SkinnedMeshDecals.PaintDecal.instance.RenderDecal(r, projector, transform.position-Vector3.forward*scale*0.5f, Quaternion.identity, Vector2.one*scale*0.5f, scale);
             }
         }
     }
