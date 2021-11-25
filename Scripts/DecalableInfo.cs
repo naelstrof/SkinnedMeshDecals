e783efc2ad90ab05333de834c4ebb019d0178bac
naelstrof <naelstrof@gmail.com>
2021-11-25 14:17:20 -0700

    removed the instance requirement of paintdecal, allow for rendering to more than one decal map
    updated readme
    
    updated readme

--- Scripts/DecalableInfo.cs
+++ Scripts/DecalableInfo.cs
@@ -5,14 +5,42 @@ using UnityEngine.Rendering;
 
 namespace SkinnedMeshDecals {
 public class DecalableInfo : MonoBehaviour {
-    private static List<Material> staticDecalables = new List<Material>();
-    private RenderTexture texture;
+    private Material[] materials;
+    private class TextureTarget {
+        private static void ClearRenderTexture(RenderTexture target) {
+            var rt = UnityEngine.RenderTexture.active;
+            UnityEngine.RenderTexture.active = target;
+            GL.Clear(true, true, Color.clear);
+            UnityEngine.RenderTexture.active = rt;
+        }
+        public TextureTarget(string textureName, int textureScale, Renderer renderer, Material[] materials) {
+            drawIndices = new List<int>();
+            decalableMaterials = new List<Material>();
+            texture = new RenderTexture(textureScale, textureScale, 0);
+            ClearRenderTexture(texture);
+            for (int i=0;i<materials.Length;i++) {
+                if (materials[i].HasProperty(textureName)) {
+                    decalableMaterials.Add(materials[i]);
+                    drawIndices.Add(i);
+                }
+            }
+        }
+        public RenderTexture texture;
+        public List<int> drawIndices;
+        public List<Material> decalableMaterials;
+    }
+    private Dictionary<string, TextureTarget> textureTargets;
     private new Renderer renderer;
-    private List<Material> decalableMaterials;
-    private List<int> drawIndices;
     public float lastUse;
-    public float memorySizeMB => texture != null ? (texture.width*texture.height*4f)/(float)(1e+6f) : 0f;
-    public bool IsValid() => texture != null;
+    public float memorySizeMB {
+        get {
+            float size = 0f;
+            foreach(var pair in textureTargets) {
+                size += (pair.Value.texture.width*pair.Value.texture.height*4f)/(float)(1e+6f);
+            }
+            return size;
+        }
+    }
     private int CeilPowerOfTwo(int v) {
         v--;
         v |= v >> 1;
@@ -24,67 +52,42 @@ public class DecalableInfo : MonoBehaviour {
         return v;
     }
     void Awake() {
-        decalableMaterials = new List<Material>();
-        drawIndices = new List<int>();
+        textureTargets = new Dictionary<string, TextureTarget>();
         renderer = GetComponent<Renderer>();
-        renderer.GetMaterials(staticDecalables);
-        foreach(Material mat in staticDecalables) {
-            if (PaintDecal.IsDecalable(mat)) {
-                decalableMaterials.Add(mat);
-            }
-        }
-        // If we've got nothing to decal to, we just quit out.
-        if (decalableMaterials.Count <= 0) {
-            return;
-        }
-        int worldScale = Mathf.RoundToInt(renderer.bounds.extents.magnitude*PaintDecal.instance.texelsPerMeter);
-        int textureScale = Mathf.Clamp(CeilPowerOfTwo(worldScale), 16, 4096);
-        texture = new RenderTexture(textureScale, textureScale, 0);
-        PaintDecal.ClearRenderTexture(texture);
-
-        foreach(Material mat in decalableMaterials) {
-            mat.SetTexture("_DecalColorMap", texture);
-        }
+        materials = renderer.materials;
         lastUse = Time.time;
-
-        int maxIndex = 1;
-        if (renderer is MeshRenderer) {
-            MeshFilter f = renderer.GetComponent<MeshFilter>();
-            if (f != null) {
-                maxIndex = f.sharedMesh.subMeshCount;
-            }
-        } else if (renderer is SkinnedMeshRenderer) {
-            SkinnedMeshRenderer sr = (SkinnedMeshRenderer)renderer;
-            maxIndex = sr.sharedMesh.subMeshCount;
-        }
-        for(int i=0;i<maxIndex;i++) {
-            // There could be other materials on this renderer that aren't related to decals (usually eyeballs on characters)
-            // We need to make sure we skip them.
-            if (staticDecalables.Count == 0 || staticDecalables.Count <= i || !PaintDecal.IsDecalable(staticDecalables[i])) {
-                continue;
-            }
-            drawIndices.Add(i);
-        }
-        PaintDecal.instance.AddDecalableInfo(this);
+        PaintDecal.AddDecalableInfo(this);
     }
-    public void Render(CommandBuffer buffer, Material projector) {
-        if (texture == null) {
-            return;
+    public void Render(CommandBuffer buffer, Material projector, string textureName) {
+        // Create the texture if we don't have it.
+        if (!textureTargets.ContainsKey(textureName)) {
+            int worldScale = Mathf.RoundToInt(renderer.bounds.extents.magnitude*PaintDecal.texelsPerMeter);
+            int textureScale = Mathf.Clamp(CeilPowerOfTwo(worldScale), 16, 4096);
+
+            textureTargets[textureName] = new TextureTarget(textureName, textureScale, renderer, materials);
+            foreach(Material mat in materials) {
+                if (mat.HasProperty(textureName)) {
+                    mat.SetTexture(textureName, textureTargets[textureName].texture);
+                }
+            }
         }
-        buffer.SetRenderTarget(texture);
-        Vector2 pixelRect = new Vector2(texture.width, texture.height);
+        TextureTarget target = textureTargets[textureName];
+        buffer.SetRenderTarget(target.texture);
+        Vector2 pixelRect = new Vector2(target.texture.width, target.texture.height);
         buffer.SetViewport(new Rect(Vector2.zero, pixelRect));
-        foreach(int drawIndex in drawIndices) {
+        foreach(int drawIndex in target.drawIndices) {
             buffer.DrawRenderer(renderer, projector, drawIndex);
         }
         lastUse = Time.time;
     }
     void OnDestroy() {
-        PaintDecal.instance.RemoveDecalableInfo(this);
-        foreach(Material mat in decalableMaterials) {
-            mat.SetTexture("_DecalColorMap", null);
+        PaintDecal.RemoveDecalableInfo(this);
+        foreach(var pair in textureTargets) {
+            foreach(Material mat in materials) {
+                mat.SetTexture(pair.Key, null);
+            }
+            pair.Value.texture.Release();
         }
-        texture.Release();
     }
 }
 
