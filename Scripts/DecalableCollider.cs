39479e94e3ebb7444ad002194c7badc789286cc2
naelstrof <naelstrof@gmail.com>
2024-09-19 22:13:23 -0600

    Such an important aspect of decaling, included DecalableCollider into the main package.

---
+++ Scripts/DecalableCollider.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace SkinnedMeshDecals {
+public class DecalableCollider : MonoBehaviour {
+    public List<Renderer> decalableRenderers;
+}
+
+}
